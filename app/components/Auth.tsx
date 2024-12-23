ddd36ed133dc9893fd03603ad9240a4435b05377
erin.miller <erinm1018@gmail.com>
2024-12-22 19:38:24 -0600

    lint: fix formatting

--- app/components/Auth.tsx
+++ app/components/Auth.tsx
@@ -1,45 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const generateRandomString = (length = 16) => {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        result += characters[randomIndex];
-    }
-    return result;
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  return result;
 };
 
 export const initOAuth = () => {
-    const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
-    const uri = 'https://scouttoon.info/';
-    const redirectUri = encodeURIComponent(uri);
-    const scope = encodeURIComponent('identify');
+  const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+  const uri = "https://scouttoon.info/";
+  const redirectUri = encodeURIComponent(uri);
+  const scope = encodeURIComponent("identify");
 
-    const randomState = generateRandomString();
-    localStorage.setItem('oauth-state', randomState);
+  const randomState = generateRandomString();
+  localStorage.setItem("oauth-state", randomState);
 
-    const url = `https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scope}&state=${btoa(randomState)}`;
-    
-    // Redirect to Discord authorization URL
-    window.location.href = url;
+  const url = `https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scope}&state=${btoa(
+    randomState
+  )}`;
+
+  // Redirect to Discord authorization URL
+  window.location.href = url;
 };
 
 const Auth = () => {
-  const clickedImg = '/images/button-clicked.png';
-  const unclickedImg = '/images/button-unclicked.png';
+  const clickedImg = "/images/button-clicked.png";
+  const unclickedImg = "/images/button-unclicked.png";
 
   const [isPressed, setIsPressed] = useState(false);
   const handleMouseDown = () => setIsPressed(true);
   const handleMouseUp = () => setIsPressed(false);
   const handleMouseLeave = () => setIsPressed(false);
-  
+
   return (
     <div className="flex w-full max-w-xl mx-auto">
       <div className="bg-white p-6 md:p-10 rounded-lg shadow-lg text-center border border-gray-300 space-y-6 md:space-y-5 mt-10">
-        <h2 className="text-3xl font-semibold font-minnie text-gray-800 mb-6">Connect to Discord</h2>
-        <p className="text-xl text-gray-600 font-impress">ToonScout needs a Discord connection to function.</p>
-        <p className="text-xl text-gray-600 font-impress">Click the button below to begin!</p>
+        <h2 className="text-3xl font-semibold font-minnie text-gray-800 mb-6">
+          Connect to Discord
+        </h2>
+        <p className="text-xl text-gray-600 font-impress">
+          ToonScout needs a Discord connection to function.
+        </p>
+        <p className="text-xl text-gray-600 font-impress">
+          Click the button below to begin!
+        </p>
         <button
           id="login"
           onMouseDown={handleMouseDown}
@@ -49,12 +58,11 @@ const Auth = () => {
           className="relative flex justify-center items-center w-16 h-16 mx-auto"
           style={{
             backgroundImage: `url(${isPressed ? clickedImg : unclickedImg})`,
-            backgroundSize: 'contain',
-            backgroundPosition: 'center',
-            cursor: 'pointer',
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+            cursor: "pointer",
           }}
-        >
-        </button>
+        ></button>
       </div>
     </div>
   );
--- app/components/GameSteps.tsx
+++ app/components/GameSteps.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import LoadingDots from './LoadingDots';
+import React from "react";
+import LoadingDots from "./LoadingDots";
 
 const GameSteps: React.FC = () => (
-    <div className="flex max-w-7xl mx-auto">
-      <div className="bg-white p-6 md:p-10 rounded-lg shadow-lg text-center border border-gray-300 space-y-6 md:space-y-5 mt-10">
-      <LoadingDots className="text-3xl font-semibold font-minnie text-gray-800 mb-4" text="Connecting to Toontown Rewritten" />
+  <div className="flex max-w-7xl mx-auto">
+    <div className="bg-white p-6 md:p-10 rounded-lg shadow-lg text-center border border-gray-300 space-y-6 md:space-y-5 mt-10">
+      <LoadingDots
+        className="text-3xl font-semibold font-minnie text-gray-800 mb-4"
+        text="Connecting to Toontown Rewritten"
+      />
       <div className="flex justify-center space-x-8">
-        <Step title="1. Enable Companion App Support" image="/images/gameplay-menu.png" />
-        <Step title="2. Click 'OK' on in-game popup and select a toon" image="/images/prompt.png" />
+        <Step
+          title="1. Enable Companion App Support"
+          image="/images/gameplay-menu.png"
+        />
+        <Step
+          title="2. Click 'OK' on in-game popup and select a toon"
+          image="/images/prompt.png"
+        />
       </div>
     </div>
   </div>
@@ -15,7 +24,9 @@ const GameSteps: React.FC = () => (
 
 const Step: React.FC<{ title: string; image: string }> = ({ title, image }) => (
   <div className="bg-gray-100 p-6 rounded-lg shadow-md border border-gray-300 max-w-xl">
-    <h2 className="text-2xl font-semibold font-minnie text-gray-800 mb-4">{title}</h2>
+    <h2 className="text-2xl font-semibold font-minnie text-gray-800 mb-4">
+      {title}
+    </h2>
     <img src={image} alt={title} className="mx-auto" />
   </div>
 );
