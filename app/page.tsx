3d2c7d755942f8328305cfa90cd978d356121183
erin.miller <erinm1018@gmail.com>
2025-01-05 17:37:31 -0600

    Fix: unnecessary var pass

--- app/page.tsx
+++ app/page.tsx
@@ -4,41 +4,30 @@ import "./styles/fonts.css";
 import GameSteps from "./components/GameSteps/GameSteps";
 import Home from "./components/Home/Home";
 import { initWebSocket } from "./api/LocalWebSocket";
-<<<<<<< HEAD
-import { initScoutWebSocket } from "./api/ScoutWebSocket";
-=======
 import { initScoutWebSocket, sendScoutData } from "./api/ScoutWebSocket";
->>>>>>> b545765f286c77b934e79fd71ad662e144f8c188
 import { useConnectionContext } from "./context/ConnectionContext";
 import { useDiscordContext } from "./context/DiscordContext";
 import { useToonContext } from "./context/ToonContext";
 
 const HomePage: React.FC = () => {
   const { isConnected, setIsConnected } = useConnectionContext();
-<<<<<<< HEAD
-  const { setToonData } = useToonContext();
-=======
   const { toonData, setToonData } = useToonContext();
->>>>>>> b545765f286c77b934e79fd71ad662e144f8c188
   const { userId } = useDiscordContext();
 
   useEffect(() => {
     initScoutWebSocket();
-    initWebSocket(setIsConnected, setToonData, userId);
+    initWebSocket(setIsConnected, setToonData);
   }, []);
 
-<<<<<<< HEAD
-=======
   useEffect(() => {
     const sendData = () => {
       if (userId && toonData) {
-	sendScoutData(userId, toonData); // Await the async call
-      } 
-     };
-     sendData();
+        sendScoutData(userId, toonData);
+      }
+    };
+    sendData();
   }, [userId, toonData]);
 
->>>>>>> b545765f286c77b934e79fd71ad662e144f8c188
   return (
     <div className="page-container">
       {/* Connecting Steps */}
