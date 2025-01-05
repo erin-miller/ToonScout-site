3ffd74531f88342742088e3ac89a28bed7419a4a
erin.miller <erinm1018@gmail.com>
2025-01-05 15:27:04 -0600

    add: anim tab to modal

--- app/components/Home/DiscordModal.tsx
+++ app/components/Home/DiscordModal.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import OAuth from "../OAuth/OAuth";
 import ArrowButton from "../ArrowButton";
 import { sendScoutData } from "@/app/api/ScoutWebSocket";
+import AnimatedTabContent from "../animations/AnimatedTab";
 
 interface DiscordModalProps {
   isOpen: boolean;
@@ -74,34 +75,36 @@ const DiscordModal: React.FC<DiscordModalProps> = ({ isOpen, onClose }) => {
 
   return (
     <div className="fixed transition ease-in-out inset-0 bg-gray-500 bg-opacity-70 dark:bg-gray-1200 dark:bg-opacity-70 dark:text-gray-100 flex justify-center items-center z-50">
-      <div className="text-gray-800 dark:text-gray-100 bg-gray-100 dark:bg-gray-1100 p-6 rounded-lg w-96 border-blue-700 border-4 relative text-xl">
-        {/* Step 1: OAuth */}
-        {step === 1 && <OAuth />}
+      <AnimatedTabContent>
+        <div className="text-gray-800 dark:text-gray-100 bg-gray-100 dark:bg-gray-1100 p-6 rounded-lg w-96 border-blue-700 border-4 relative text-xl">
+          {/* Step 1: OAuth */}
+          {step === 1 && <OAuth />}
 
-        {/* Step 2: Add Bot */}
-        {step === 2 && (
-          <div>
-            <h3 className="text-2xl mb-2 minnie-title">You're connected!</h3>
-            <p className="py-2">
-              Click the button below to add the bot to your Discord account.
-            </p>
-            <div className="space-x-2">
-              <ArrowButton onClick={handleClick} />
+          {/* Step 2: Add Bot */}
+          {step === 2 && (
+            <div>
+              <h3 className="text-2xl mb-2 minnie-title">You're connected!</h3>
+              <p className="py-2">
+                Click the button below to add the bot to your Discord account.
+              </p>
+              <div className="space-x-2">
+                <ArrowButton onClick={handleClick} />
+              </div>
+
+              {/* Error Message */}
+              {error && <p className="text-red-500 pt-2">{error}</p>}
             </div>
+          )}
 
-            {/* Error Message */}
-            {error && <p className="text-red-500 pt-2">{error}</p>}
-          </div>
-        )}
-
-        {/* Close button in the top-right corner */}
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-2xl text-blue-700 hover:text-blue-900"
-        >
-          &times;
-        </button>
-      </div>
+          {/* Close button in the top-right corner */}
+          <button
+            onClick={onClose}
+            className="absolute top-2 right-2 text-2xl text-blue-700 hover:text-blue-900"
+          >
+            &times;
+          </button>
+        </div>
+      </AnimatedTabContent>
     </div>
   );
 };
