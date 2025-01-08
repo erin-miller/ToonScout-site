3ecfda4f8f2f5746f545747b3e4cf3ff699f583e
erin.miller <erinm1018@gmail.com>
2025-01-08 13:28:22 -0600

    tasks: add task png

--- app/components/Home/tabs/TasksTab.tsx
+++ app/components/Home/tabs/TasksTab.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { TabProps } from "./components/TabComponent";
+import AnimatedTabContent from "../../animations/AnimatedTab";
 
 const TasksTab: React.FC<TabProps> = ({ toonData }) => {
   return (
-    <div>
-      <p>Sorry, this page is under construction. Come back later!</p>
-    </div>
+    <AnimatedTabContent>
+      <div className="grid grid-rows-2 grid-cols-2">
+        <div className="task-container left-task">
+          <img src="/images/task.png" alt="Task 1" className="task"></img>
+        </div>
+        <div className="task-container left-task">
+          {" "}
+          <img src="/images/task.png" alt="Task 2" className="task"></img>
+        </div>
+        <div className="task-container right-task">
+          {" "}
+          <img src="/images/task.png" alt="Task 3" className="task"></img>
+        </div>
+        <div className="task-container right-task">
+          {" "}
+          <img src="/images/task.png" alt="Task 4" className="task"></img>
+        </div>
+      </div>
+    </AnimatedTabContent>
   );
 };
 
